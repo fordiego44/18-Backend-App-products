5e38d7aae961f7d0a4ff69fb20ac7318aa3b4fc4
Diego Armando <fordiego44@gmail.com>
2025-04-24 11:00:50 -0500

    backend products de la seccion 18 y las demas actualizaciones que hubo durantes las siguientes secciones

---
+++ src/main/java/com/diego/curso/springboot/backend/backend_products/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.diego.curso.springboot.backend.backend_products.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.diego.curso.springboot.backend.backend_products.entities.Product;
+
+@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:4200"})
+@RepositoryRestResource(path = "products")
+public interface ProductRepository extends CrudRepository<Product, Long>  {
+
+}
